1007210d1d7a19ba7bdca0d749774bd92a5d2fc0
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-10-21 16:15:32 +0900

    gird

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,23 +1,54 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Col, Input, Menu, Row } from "antd";
 
 const NodeBird = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal">
         <Menu.Item>
-          <Link href="/">노드버드</Link>
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
         </Menu.Item>
+
         <Menu.Item>
-          <Link href="/profile">프로필</Link>
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
         </Menu.Item>
+
         <Menu.Item>
-          <Link href="/signup">회원가입</Link>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+
+        <Menu.Item>
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
         </Menu.Item>
       </Menu>
-      {children}
+
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.4leaf-software.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            4leaf
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
