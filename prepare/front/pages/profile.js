e83dfb3f5bdbeb94b7b28bf355c150eef9e704f0
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-11-18 18:36:31 +0900

    profile

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Head from "next/head";
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+  const followerList = [
+    { nickname: "멍청이" },
+    { nickname: "똥개" },
+    { nickname: "말미잘" },
+  ];
+  const followingList = [
+    { nickname: "멍청이" },
+    { nickname: "똥개" },
+    { nickname: "말미잘" },
+  ];
+
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>Profile</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
