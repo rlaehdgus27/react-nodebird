3f1f3d1adb414bd1dda1d076ccb28bacf8b6d717
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-11-19 19:40:20 +0900

    redux

--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -2,8 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import "antd/dist/antd.css";
+import wrapper from "../store/configureStore";
 
-const App = ({ Component }) => {
+const NodeBird = ({ Component }) => {
   return (
     <>
       <Head>
@@ -15,8 +16,8 @@ const App = ({ Component }) => {
   );
 };
 
-App.propTypes = {
+NodeBird.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default App;
+export default wrapper.withRedux(NodeBird);
