b14b3ca1018a5edb34c0e3fbc5a9f1d69a616b1c
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-11-22 19:56:58 +0900

    middleware

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -1,3 +1,5 @@
+import { HYDRATE } from "next-redux-wrapper";
+
 const initialState = {
   user: {
     isLoggedIn: false,
@@ -25,6 +27,9 @@ export const logoutAction = () => {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case HYDRATE:
+      return { ...state, ...action.payload };
+
     case "LOG_IN":
       return {
         ...state,
@@ -44,6 +49,9 @@ const rootReducer = (state = initialState, action) => {
           user: null,
         },
       };
+
+    default:
+      return state;
   }
 };
 
