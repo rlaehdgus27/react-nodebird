a493b1b688df7d660f2bcfd37a404d420f5919ef
rlaehdgus27 <rlaehdgus27@gmail.com>
2022-01-10 22:18:18 +0900

    image

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -2,7 +2,47 @@ import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import { PlusOutlined } from "@ant-design/icons";
 
-const PostImages = () => {
+const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          width="50%"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          width="50%"
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
   return <div>구현중...</div>;
 };
 
